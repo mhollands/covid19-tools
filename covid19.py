8f4e83df23f70757dd6c6afb9750979d74493ff4
Matt Hollands <mattyh999@hotmail.com>
2020-03-28 16:15:17 -0700

    added delta option to plot change per day

--- covid19.py
+++ covid19.py
@@ -3,23 +3,30 @@ import core
 import os
 import matplotlib.pyplot as plt
 import pandas as pd
-pd.plotting.register_matplotlib_converters(explicit=False)
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
 
 def plot_cases_by_date(data_dict):
 	status_options = ["confirmed", "deaths", "recovered"]
 	for status in status_options:
-		plt.plot(data_dict[status]["dates"], data_dict[status]["cases"])
+		dates = data_dict[status]["dates"]
+		cases = data_dict[status]["cases"]
+		if(delta):
+			cases = [cases[i] - cases[i-1] for i in range(1, len(cases))]
+			dates = dates[1:]
+		plt.plot(dates, cases)
 	plt.legend(status_options,loc='upper left')
 	plt.xticks(rotation='vertical')
 	plt.tight_layout()
 	plt.show()
 
-opts, args = getopt.getopt(sys.argv[1:],"hc:p:sg",["help","country=","province=","save","graph"])
+opts, args = getopt.getopt(sys.argv[1:],"hc:p:sgd",["help","country=","province=","save","graph","delta"])
 
 graph = False
 save=False
 province = None
 country = None
+delta=False
 for opt in opts:
 	if(opt[0] in ("-h", "--help")):
 		print("syntax: --country <country> [--province <province>] [--outfile <outputfile>]")
@@ -32,6 +39,8 @@ for opt in opts:
 		save = True
 	if(opt[0] in ("-g", "--graph")):
 		graph = True
+	if(opt[0] in ("-d", "--delta")):
+		delta = True
 
 if(country is None):
 	print("Must specify a country")
